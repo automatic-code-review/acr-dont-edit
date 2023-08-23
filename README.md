5e65960d7e4e2fa7785591e98397f1bc494dce9f
kielsonzinn <kielsonzinn@gmail.com>
2023-08-23 11:04:19 -0300

    create extension acr-dont-edit

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
-# acr-extension-name
+# acr-dont-edit
+
+Extensao que verifica se um arquivo que não deve ser editado, foi editado, se sim adiciona um comentario personalizado
+conforme o
+configurado
 
 Arquivo config.json
 
 ```json
 {
-
+  "stage": "static",
+  "rules": [
+    {
+      "message": "Don't edit, please",
+      "regexPath": [
+        ".*\/example.sql"
+      ]
+    }
+  ]
 }
 ```
--- src/review.py
+++ src/review.py
@@ -1,24 +1,43 @@
-def review(config):
-    path_target = config['path_target']
-    path_source = config['path_source']
+import hashlib
+import re
 
-    merge = config['merge']
-    project_id = merge['project_id']
-    merge_request_id = merge['merge_request_id']
-    
+
+def review(config):
+    rules = config["rules"]
+    changes = config["merge"]["changes"]
+
     comments = []
-    
-    # TODO IMPLEMENTAR EXTENSION
-    #  O OBJETO DE COMENTARIO DEVE POSSUIR O SEGUINTE FORMATO
-    #  {
-    #      "id": "",
-    #      "comment": "",
-    #      "position": {
-    #          "language": "",
-    #          "path': "",
-    #          "startInLine": 0,
-    #          "endInLine": 0
-    #    }
-    #  }
+
+    for rule in rules:
+        message = rule["message"]
+        regex_path_list = rule["regexPath"]
+
+        for change in changes:
+            if not change["new_file"]:
+                path = change["new_path"]
+
+                for regex_path in regex_path_list:
+                    if re.search(regex_path, path):
+                        comments.append(__create_comment(message, path))
+                        break
 
     return comments
+
+
+def __create_comment(message, path):
+    comment = {
+        "id": __generate_md5(path),
+        "comment": message,
+        "position": {
+            "path": path,
+            "snipset": False
+        }
+    }
+
+    return comment
+
+
+def __generate_md5(string):
+    md5_hash = hashlib.md5()
+    md5_hash.update(string.encode('utf-8'))
+    return md5_hash.hexdigest()
